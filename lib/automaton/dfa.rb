c1c9777134c7aa76241d6363ab5d5c5afea12604
shozawa <tomohiro.shozawa@gmail.com>
2019-05-12 10:53:58 +0900

    DFAクラスを追加

--- lib/automaton/dfa.rb
+++ lib/automaton/dfa.rb
@@ -1,3 +1,13 @@
+class DFA < Struct.new(:current_state, :accept_states, :rulebook)
+  def accepting?
+    accept_states.include?(current_state)
+  end
+
+  def read_character(character)
+    self.current_state = rulebook.next_state(current_state, character)
+  end
+end
+
 class FARule < Struct.new(:state, :character, :next_state)
   def apply_to?(state, character)
     self.state == state && self.character == character
@@ -16,6 +26,8 @@ class DFARuleBook < Struct.new(:rules)
   def next_state(state, character)
     rule_for(state, character).follow
   end
+
+  private
 
   def rule_for(state, character)
     rules.detect { |rule| rule.apply_to?(state, character) }
--- tests/automaton/test_dfa.rb
+++ tests/automaton/test_dfa.rb
@@ -17,3 +17,29 @@ class TestFARuleBook < Test::Unit::TestCase
     assert_equal(@rule_book.next_state(2, 'b'), 3)
   end
 end
+
+class TestDFA < Test::Unit::TestCase
+  def setup
+    rule_book = DFARuleBook.new([
+      FARule.new(1, 'a', 2),
+      FARule.new(2, 'b', 3),
+      FARule.new(3, 'c', 4),
+    ])
+    @dfa = DFA.new(1, [4], rule_book)
+  end
+  
+  def test_accepting?
+    assert_equal(DFA.new(1, [1, 3], nil).accepting?, true)
+    assert_equal(DFA.new(1, [2, 3], nil).accepting?, false)
+  end
+
+  def test_simple_case
+    assert_equal(@dfa.accepting?, false)
+    @dfa.read_character('a')
+    assert_equal(@dfa.accepting?, false)
+    @dfa.read_character('b')
+    assert_equal(@dfa.accepting?, false)
+    @dfa.read_character('c')
+    assert_equal(@dfa.accepting?, true)
+  end
+end
