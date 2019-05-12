0bef86fa722f6af3541128cccf48f8cc912dc73c
shozawa <tomohiro.shozawa@gmail.com>
2019-05-12 19:55:17 +0900

    FARuleを抽出

--- lib/automaton/dfa.rb
+++ lib/automaton/dfa.rb
@@ -1,3 +1,5 @@
+require_relative './fa_rule.rb'
+
 class DFADesign < Struct.new(:current_state, :accept_states, :rulebook)
   def to_dfa
     DFA.new(current_state, accept_states, rulebook)
@@ -23,20 +25,6 @@ class DFA < Struct.new(:current_state, :accept_states, :rulebook)
 
   def read_character(character)
     self.current_state = rulebook.next_state(current_state, character)
-  end
-end
-
-class FARule < Struct.new(:state, :character, :next_state)
-  def apply_to?(state, character)
-    self.state == state && self.character == character
-  end
-
-  def follow
-    next_state
-  end
-
-  def inspect
-    "#<FARule #{state.inspect} --#{character}--> #{next_state.inspect}>"
   end
 end
 
