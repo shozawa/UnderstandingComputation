4b32b1a0c62cf8700afe44da7529e6cbce5152c5
shozawa <tomohiro.shozawa@gmail.com>
2019-05-12 17:42:03 +0900

    rubocop -x

--- tests/automaton/test_dfa.rb
+++ tests/automaton/test_dfa.rb
@@ -3,12 +3,12 @@ require 'test/unit'
 require './lib/automaton/dfa.rb'
 
 class TestFARuleBook < Test::Unit::TestCase
-  def setup 
+  def setup
     @rule_book = DFARuleBook.new([
-      FARule.new(1, 'a', 2),
-      FARule.new(2, 'a', 2),
-      FARule.new(2, 'b', 3),
-    ])
+                                   FARule.new(1, 'a', 2),
+                                   FARule.new(2, 'a', 2),
+                                   FARule.new(2, 'b', 3),
+                                 ])
   end
 
   def test_simple_case
@@ -21,13 +21,13 @@ end
 class TestDFA < Test::Unit::TestCase
   def setup
     rule_book = DFARuleBook.new([
-      FARule.new(1, 'a', 2),
-      FARule.new(2, 'b', 3),
-      FARule.new(3, 'c', 4),
-    ])
+                                  FARule.new(1, 'a', 2),
+                                  FARule.new(2, 'b', 3),
+                                  FARule.new(3, 'c', 4),
+                                ])
     @dfa = DFA.new(1, [4], rule_book)
   end
-  
+
   def test_accepting?
     assert_equal(DFA.new(1, [1, 3], nil).accepting?, true)
     assert_equal(DFA.new(1, [2, 3], nil).accepting?, false)
