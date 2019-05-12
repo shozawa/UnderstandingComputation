55ba10ef72e79fa29e3da13e504462a6931bcb8f
shozawa <tomohiro.shozawa@gmail.com>
2019-05-13 08:07:33 +0900

    NFAを追加

--- lib/automaton/nfa.rb
+++ lib/automaton/nfa.rb
@@ -2,6 +2,22 @@ require 'set'
 
 require_relative './fa_rule.rb'
 
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
+  def accepting?
+    (current_states & accept_states).any?
+  end
+
+  def read_character(character)
+    self.current_states = rulebook.next_states(current_states, character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+end
+
 class NFARuleBook < Struct.new(:rules)
   def next_states(states, character)
     states.flat_map { |state| follow_rules_for(state, character) }.to_set
--- tests/automaton/test_nfa.rb
+++ tests/automaton/test_nfa.rb
@@ -4,9 +4,9 @@ require 'test/unit'
 require './lib/automaton/fa_rule.rb'
 require './lib/automaton/nfa.rb'
 
-class TestNFARuleBook < Test::Unit::TestCase
+class TestNFA < Test::Unit::TestCase
   def test_simple_case
-    rules = NFARuleBook.new(
+    rulebook = NFARuleBook.new(
       [
         FARule.new(1, 'a', 2),
         FARule.new(2, 'a', 100),
@@ -14,6 +14,22 @@ class TestNFARuleBook < Test::Unit::TestCase
         FARule.new(3, 'c', 3)
       ]
     )
-    assert_equal(Set.new([2, 100]), rules.next_states([1, 2], 'a'))
+    nfa = NFA.new(Set.new([1]), Set.new([100]), rulebook)
+    nfa.read_string('aa')
+    assert_equal(true, nfa.accepting?)
+  end
+end
+
+class TestNFARuleBook < Test::Unit::TestCase
+  def test_simple_case
+    rulebook = NFARuleBook.new(
+      [
+        FARule.new(1, 'a', 2),
+        FARule.new(2, 'a', 100),
+        FARule.new(2, 'b', 3),
+        FARule.new(3, 'c', 3)
+      ]
+    )
+    assert_equal(Set.new([2, 100]), rulebook.next_states([1, 2], 'a'))
   end
 end
